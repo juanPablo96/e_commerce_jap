d3ab53711870359710ba921d91625f1c18e4463e
juanPablo96 <juan.pablo1996@hotmail.com>
2021-08-22 11:45:10 -0300

    Entrega_1

--- js/login.js
+++ js/login.js
@@ -3,4 +3,20 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
+    document.getElementById("login").addEventListener("click",redireccionar);
+  function redireccionar(){
+    let email = document.getElementById("email").value
+    let contrasenia = document.getElementById("password").value
+
+    if (email != "" && contrasenia != "") {
+
+        window.location.href = "home.html"
+        
+    }else{
+
+        alert("Debe completar los datos solicitados");
+    }
+
+  } 
+
 });
--- js/products.js
+++ js/products.js
@@ -3,4 +3,45 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
+    // 1. obtener la información. Para eso usamos la función FETCH o la función GETJSONDATA
+
+
+
+async function showProductList(){
+    
+    const products_link = await getJSONData(PRODUCTS_URL);
+
+    let htmlContentToAppend = "";
+    
+    for(product of products_link.data){
+        
+
+            htmlContentToAppend += `
+            <a href="category-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <small class="text-muted">` + product.soldCount + ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p>
+                        <p class="mb-1">` + product.currency + ` `+`` +  product.cost + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+
+            
+        }
+        
+        document.getElementById("products-list-container").innerHTML = htmlContentToAppend;
+
+
+    }
+
+    showProductList();
+
 });
